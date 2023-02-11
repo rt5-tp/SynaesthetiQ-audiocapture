9be723944e1de8d7be863625ae9e115824e0aefe
ross <ross.mckeown1@gmail.com>
2023-02-11 19:46:09 +0000

    updates

--- backup/sdlcallback.cpp
+++ backup/sdlcallback.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <alsa/asoundlib.h>
 #include <fstream>
+#include <SDL2/SDL.h>
 
 
 std::ofstream audioFile("audio.raw", std::ios::binary);
+SDL_Renderer* renderer;
+
 
 void MyCallback(snd_async_handler_t *pcm_callback)
 {
@@ -24,10 +27,7 @@ void MyCallback(snd_async_handler_t *pcm_callback)
     std::cout << "Avail = " << avail << std::endl;
     
     static char buffer[4096];
-    
-    // // Allocate memory for audio data
-    // const int buffer_size = 1024; //reduce this
-    // short* buffer = new short[buffer_size];
+
     
     snd_pcm_sframes_t frames = snd_pcm_readi(handle, buffer, avail);
 
@@ -40,11 +40,58 @@ void MyCallback(snd_async_handler_t *pcm_callback)
     }
     // Process the captured audio data in 'buffer'
     audioFile.write((char*) buffer, avail * sizeof(short));
+    // int buffer_size = 4096;
+    // Clear the screen
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+
+    // Set the color for the waveform
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+
+    // Calculate the vertical scale factor to fit the waveform in the window
+    float scale_factor = (float)600 / ((float)32768/4);
+        
+    int num_samples = avail / sizeof(int16_t);;
+    std::cout << num_samples << std::endl;
+
+    // Draw the waveform
+    for (int i = 0; i < num_samples - 1; i++) {
+        int x1 = (int)((float)i / (float)num_samples * 800.0f);
+        int y1 = (int)((float)buffer[i] * scale_factor + 300.0f);
+        int x2 = (int)((float)(i + 1) / (float)num_samples * 800.0f);
+        int y2 = (int)((float)buffer[i + 1] * scale_factor + 300.0f);
+        SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+    }  
+    SDL_RenderPresent(renderer);
+
 }
 
 int main()
 {
     snd_pcm_t *handle;
+
+    
+    // Initialize SDL2
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "Error initializing SDL2: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    // Create the SDL2 window and renderer
+    SDL_Window* window = SDL_CreateWindow("Audio Waveform", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::cerr << "Error creating SDL2 window: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+    
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == nullptr) {
+        std::cerr << "Error creating SDL2 renderer: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    
+    // can't use 'default' as it is a virtual device
     int err = snd_pcm_open(&handle, "plughw:CARD=webcam,DEV=0", SND_PCM_STREAM_CAPTURE, 0);
     if (err < 0)
     {
@@ -86,9 +133,30 @@ int main()
 
     while (true)
     {
+        // Check for keyboard events
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+            exit(0);
+            } else if (event.type == SDL_KEYDOWN) {
+            if (event.key.keysym.sym == SDLK_q) {
+                exit(0);
+            }
+            }
+        }
         // Wait for audio data to be captured and processed by the callback function
     }
 
+   // Clean up
+    // delete[] buffer;
+
+    // Clean up SDL2
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+
+    audioFile.close();
     snd_pcm_close(handle);
     return 0;
 }
